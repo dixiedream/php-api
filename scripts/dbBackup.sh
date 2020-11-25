b2de4ba59cb310536a66d57dc423795a3801fc33
Alessandro <alessandro.lucarini@smanapp.com>
2020-11-25 14:22:28 +0100

    Scripts refactoring

--- scripts/dbBackup.sh
+++ scripts/dbBackup.sh
@@ -5,7 +5,7 @@ composefile=${1:-docker-compose.yml}
 db=${DB_DATABASE:-phpboilerplate}
 
 echo "Dumping database..."
-docker-compose -f "$composefile" exec database sh -c 'exec mysqldump -p $DB_DATABASE -uroot -p"$MYSQL_ROOT_PASSWORD"' > "./$db.sql"
+docker-compose -f "$composefile" exec database sh -c 'exec mysqldump -p $DB_DATABASE -uroot -p"$MYSQL_ROOT_PASSWORD"' > "./$DB_DATABASE.sql"
 
 echo "Compressing..."
 gzip -f "./$db.sql"
--- scripts/dbRestore.sh
+++ scripts/dbRestore.sh
@@ -8,4 +8,4 @@ echo "Extracting..."
 [[ -f "./$dbname.sql.gz" ]] && gunzip "./$dbname.sql.gz"
 
 echo "Restoring..."
-docker-compose -f "$composefile" exec -T database sh -c 'exec mysql -uroot -p"$MYSQL_ROOT_PASSWORD" $DB_DATABASE' < "./$dbname.sql"
+docker-compose -f "$composefile" exec -T database sh -c 'exec mysql -uroot -p"$MYSQL_ROOT_PASSWORD" $DB_DATABASE' < "./$DB_DATABASE.sql"
